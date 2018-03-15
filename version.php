c5de37ff12da67c63e8af5dce08fc2012c68ff08
Dey Bendifallah <d.bendifallah@gmail.com>
2018-03-15 15:44:00 +0100

    Update version.php

--- version.php
+++ version.php
@@ -1,6 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
+$plugin->release = '1.0 (Build: 2018031500)';
 $plugin->version   = 2018021500;
 $plugin->requires  = 2015051109;
 $plugin->component = 'block_xapi_lrs';
+$plugin->maturity = MATURITY_STABLE;
 ?>
