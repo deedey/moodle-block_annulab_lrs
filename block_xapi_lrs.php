56a0f64c143c28e5c7e80ed937bde0eb30d09552
Dey Bendifallah <d.bendifallah@gmail.com>
2018-03-15 09:22:43 +0100

    Update block_xapi_lrs.php

--- block_xapi_lrs.php
+++ block_xapi_lrs.php
@@ -7,20 +7,24 @@ class block_xapi_lrs extends block_base {
 
     public function get_content() {
         global $USER, $_SERVER, $DB;
-        $TinCanExist = $DB->count_records_select('config_plugins','name = "tincanlaunchlrsendpoint" AND value = "http://lrs.annulab.com/xapi/"');
+        $TinCanExist = $DB->count_records_select('config_plugins','name = "endpoint" AND value = "http://lrs.annulab.com/xapi/"');
         $this->content = new stdClass();
         if ($TinCanExist == 0)
             $this->content->text = html_writer::div('<span style="color:#FF0000;font-weight: bold;"'.
-                 ' title="Exige les plugins xAPI, logstore_xapi et TincanLauch">LrsAnnulab non actif</span>');
+                 ' title="Exige le plugins logstore_xapi">LrsAnnulab non actif</span>');
         else
         {
-           $flag = ((strstr($USER->institution, '-20') || $USER->phone1 == '') && $USER->address == '') ? 0 : 1;
-           $urlBase = 'http://lrs.annulab.com/MesDatas.php';
+           $Tab = array();
+           $Tab = explode(',',$CFG->siteadmins);
+           $IsTeacher = $DB->count_records_select('role_assignments','userid = '.$USER->id.' AND (roleid = 1 OR roleid = 3)');
+           $flag = ($IsTeacher > 0) ? 1 : 0;
+           for ($i=0;$i< count($Tab);$i++){if ($USER->id == $Tab[$i]) $flag=2;}
+           $urlBase = 'http://formagri.educagri.fr/xapilrs/MesDatas.php';
            $LeNom = $USER->firstname." ".$USER->lastname;
            $this->content = new stdClass();
            $url = new moodle_url($urlBase, ['flag'=>$flag,'nom'=>$LeNom,'LMSorigin'=>$_SERVER['HTTP_HOST']]);
            $this->content->text = html_writer::link($url,"Mon historique personnel",array('target' => '_blank'));
-        }
+       }
         $this->content->footer = '';
         return $this->content;
     }
