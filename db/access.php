6baa77d560cea6718cd177bad3b9faf4df59ce98
Dey Bendifallah <d.bendifallah@gmail.com>
2018-03-16 07:33:35 +0100

    Update access.php

--- db/access.php
+++ db/access.php
@@ -1,7 +1,7 @@
 <?php
     $capabilities = array(
 
-    'block/xapi_lrs:myaddinstance' => array(
+    'block/annulab_lrs:myaddinstance' => array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
@@ -11,7 +11,7 @@
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
 
-    'block/xapi_lrs:addinstance' => array(
+    'block/annulab_lrs:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
 
         'captype' => 'write',
