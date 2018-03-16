a5f51d37d3ebfca02930133808cdd5d8f03e3423
Dey Bendifallah <d.bendifallah@gmail.com>
2018-03-16 07:48:54 +0100

    Update version.php

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
-$plugin->release = '1.0 (Build: 2018031500)';
+$plugin->release = '1.0.1 (Build: 2018031500)';
 $plugin->version   = 2018021500;
 $plugin->requires  = 2015051109;
 $plugin->component = 'block_annulab_lrs';
