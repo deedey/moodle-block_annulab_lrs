8e39f8288a1c62294bdc67a640651702c698edde
Dey Bendifallah <d.bendifallah@gmail.com>
2018-04-20 13:48:53 +0200

    Update block_annulab_lrs.php

--- block_annulab_lrs.php
+++ block_annulab_lrs.php
@@ -33,22 +33,24 @@ class block_annulab_lrs extends block_base {
 
     public function get_content() {
         global $USER, $_SERVER, $DB;
-        $TinCanExist = $DB->count_records_select('config_plugins','(name = "endpoint" OR name = "tincanlaunchlrsendpoint") AND value = "http://lrs.annulab.com/xapi/"');
+        $TinCanExist = $DB->count_records_select('config_plugins','name = "endpoint" AND value = "http://lrsdata.com/xapi/"');
         $this->content = new stdClass();
         if ($TinCanExist == 0)
             $this->content->text = html_writer::div('<span style="color:#FF0000;font-weight: bold;"'.
                  ' title="'.get_string('annulab_lrs_nolrsplug', 'block_annulab_lrs').'">'.get_string('annulablrs_isdisabled', 'block_annulab_lrs').'</span>');
         else
         {
+           $cod = $DB->get_record('config_plugins',array('plugin'=>'logstore_xapi', 'name' =>'username'));
+           $codage = urlencode($cod->value);
            $Tab = array();
            $Tab = explode(',',$CFG->siteadmins);
            $IsTeacher = $DB->count_records_select('role_assignments','userid = '.$USER->id.' AND (roleid = 1 OR roleid = 3)');
            $flag = ($IsTeacher > 0) ? 1 : 0;
            for ($i=0;$i< count($Tab);$i++){if ($USER->id == $Tab[$i]) $flag=2;}
-           $urlBase = 'http://lrs.annulab.com/MesDatas.php';
+           $urlBase = 'http://lrsdata.com/MesDatas.php';
            $LeNom = $USER->firstname." ".$USER->lastname;
            $this->content = new stdClass();
-           $url = new moodle_url($urlBase, ['flag'=>$flag,'nom'=>$LeNom,'LMSorigin'=>$_SERVER['HTTP_HOST']]); 
+           $url = new moodle_url($urlBase, ['flag'=>$flag,'nom'=>$LeNom,'LMSorigin'=>$_SERVER['HTTP_HOST'],'codage'=>$codage]); 
            $this->content->text = html_writer::link($url,get_string('annulablrs', 'block_annulab_lrs'),array('target' => '_blank'));
        }
         $this->content->footer = '';
