7a7529eb7b3b156a37a1002c7fa33e20dccc0ecf
Dey Bendifallah <d.bendifallah@gmail.com>
2018-05-23 11:06:08 +0200

    Update block_annulab_lrs.php

--- lang/en/block_annulab_lrs.php
+++ lang/en/block_annulab_lrs.php
@@ -2,6 +2,7 @@
 $string['pluginname'] = 'My own Datas';
 $string['annulablrs'] = 'My own Datas';
 $string['annulablrs:addinstance'] = 'Add un block My own Datas';
+$string['annulablrs:myaddinstance'] = 'Add un block My own Datas';
 $string['annulablrs_nolrsplug'] = 'Needs Logstore_xapi plugin';
 $string['annulablrs_isdisabled'] = 'LrsAnnulab is disabled';
 ?>
