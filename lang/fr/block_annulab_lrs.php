848bd42133d30276d58096269534374688fca1f0
Dey Bendifallah <d.bendifallah@gmail.com>
2018-05-23 11:06:39 +0200

    Update block_annulab_lrs.php

--- lang/fr/block_annulab_lrs.php
+++ lang/fr/block_annulab_lrs.php
@@ -2,6 +2,7 @@
 $string['pluginname'] = 'Mon historique';
 $string['annulablrs'] = 'Mon Historique';
 $string['annulablrs:addinstance'] = 'Ajouter un block Mon historique';
+$string['annulablrs:myaddinstance'] = 'Ajouter un block Mon historique';
 $string['annulablrs_nolrsplug'] = 'Exige le plugin logstore_xapi';
 $string['annulablrs_isdisabled'] = 'LrsAnnulab est inactif';
 ?>
