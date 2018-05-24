8d174e148ddfd9ffd0a274f999672c60c2a99550
Dey Bendifallah <d.bendifallah@gmail.com>
2018-05-24 11:16:17 +0200

    Update version.php

--- version.php
+++ version.php
@@ -1,5 +1,30 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// This file needs a free subscription to http://lrs.annulab.com
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * annulab_lrs block must be used vith Annulab LRS
+ *
+ * @package    block_annulab_lrs
+ * @copyright  2018 Dey Bendifallah
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
+
 $plugin->release = '1.0.2 (Build: 2018041100)';
 $plugin->version   = 2018041100;
 $plugin->requires  = 2015051109;
