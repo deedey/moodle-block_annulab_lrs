966d20e9c0659c90a9575af54925bdba33f84d67
Dey Bendifallah <d.bendifallah@gmail.com>
2018-05-25 08:19:15 +0200

    Update block_annulab_lrs.php

--- lang/en/block_annulab_lrs.php
+++ lang/en/block_annulab_lrs.php
@@ -1,7 +1,30 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+* annulab_lrs block must be used vith Annulab LRS
+*
+* @package    block_annulab_lrs
+* @copyright  2018 Dey Bendifallah
+* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
 $string['pluginname'] = 'Annulab LRS';
 $string['annulablrs'] = 'Annulab LRS';
-$string['annulablrs:addinstance'] = 'Add un block Annulab LRS';
-$string['annulablrs:myaddinstance'] = 'Add un block Annulab LRS';
+$string['annulablrs:addinstance'] = 'Add a new Annulab LRS block';
+$string['annulablrs:myaddinstance'] = 'Add a new Annulab LRS block to dashboard';
 $string['annulablrs_nolrsplug'] = 'Needs Logstore_xapi plugin';
 $string['annulablrs_isdisabled'] = 'AnnulabLRS is disabled';
