45de33e474aa2e1adb9fc1218c16dd1e38e9df48
Dey Bendifallah <d.bendifallah@gmail.com>
2018-05-25 07:51:45 +0200

    Update block_annulab_lrs.php

--- block_annulab_lrs.php
+++ block_annulab_lrs.php
@@ -1,21 +1,19 @@
 <?php
-/** This file is part of Moodle - http://moodle.org/
- * 
- * This file needs a free subscription to http://lrs.annulab.com
- * 
- * Moodle is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Moodle is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
- */
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
 * annulab_lrs block must be used vith Annulab LRS
 *
@@ -39,9 +37,12 @@ class block_annulab_lrs extends block_base {
             return $this->content;
         }
         
-        $tincanexist = $DB->count_records_select('config_plugins', 'name = "endpoint" AND value = "http://lrsdata.com/xapi/"');
+        $endpoint = get_config('block_annulab_lrs', 'endpoint');
+        if ($endpoint == "http://lrsdata.com/xapi/") {
+            $tincanexist = true;
+        }        
         $this->content = new stdClass();
-        if ($tincanexist == 0)
+        if (!isset($tincanexist))
         {
             $this->content->text = html_writer::div('<span style="color:#FF0000;font-weight: bold;"'.
                  ' title="'.get_string('annulab_lrs_nolrsplug', 'block_annulab_lrs').'">'.
