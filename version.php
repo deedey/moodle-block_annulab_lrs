2e7f2ef291cf9215a529be6b31ed6c19b04b9d27
Dey Bendifallah <d.bendifallah@yahoo.com>
2020-08-06 15:56:26 +0200

    Update version.php
    Annulab LRS is now available for Moodle 3.9

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = '1.1.2 (Build: 2020030300)';
-$plugin->version   = 2020030300;
+$plugin->release = '1.1.3 (Build: 2020080600)';
+$plugin->version   = 2020080600;
 $plugin->requires  = 2015051109;
 $plugin->component = 'block_annulab_lrs';
 $plugin->maturity = MATURITY_STABLE;
